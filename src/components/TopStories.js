12a8faa7cadcb56d87659fb74ccbb153b677bf54
Isabel Hong <hong.isabel@gmail.com>
2021-02-14 13:34:06 -0300

    added getTopArticles deps in TopStories.js

--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -28,7 +28,7 @@ function TopStories({ loading, topStories, getTopArticles }) {
     const classes = useStyles();
     useEffect(() => {
         getTopArticles('world');
-    }, []);
+    }, [getTopArticles]);
 
     return (
         <>
@@ -36,28 +36,28 @@ function TopStories({ loading, topStories, getTopArticles }) {
                 (
                     <Spinner /> // or instead just type "Loading"
                 ) : (
-                <>
-                    <div className={classes.buttons}>
-                        <Button onClick={() => { getTopArticles('world'); }} variant="outlined" color="primary">World News</Button>
-                        <Button onClick={() => { getTopArticles('technology'); }} variant="outlined" color="secondary">Technology</Button>
-                        <Button onClick={() => { getTopArticles('us'); }} variant="outlined" color="default">US News</Button>
-                    </div>
+                    <>
+                        <div className={classes.buttons}>
+                            <Button onClick={() => { getTopArticles('world'); }} variant="outlined" color="primary">World News</Button>
+                            <Button onClick={() => { getTopArticles('technology'); }} variant="outlined" color="secondary">Technology</Button>
+                            <Button onClick={() => { getTopArticles('us'); }} variant="outlined" color="default">US News</Button>
+                        </div>
 
-                    <NavLink to="/">
-                        <Link component="button" variant="body2">Go Back</Link>
-                    </NavLink>
+                        <NavLink to="/">
+                            <Link component="button" variant="body2">Go Back</Link>
+                        </NavLink>
 
-                    <div className={classes.root} >
-                        <Grid container spacing={3}>
-                            {topStories.map((topstory) => (
-                                <Grid item xs={12} sm={4} key={topstory.url}>
-                                    <TopStory topstory={topstory} />
-                                </Grid>
-                            ))}
-                        </Grid>
-                    </div>
-                </>
-            )}
+                        <div className={classes.root} >
+                            <Grid container spacing={3}>
+                                {topStories.map((topstory) => (
+                                    <Grid item xs={12} sm={4} key={topstory.url}>
+                                        <TopStory topstory={topstory} />
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </div>
+                    </>
+                )}
         </>
     );
 }
